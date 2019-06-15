de41a9db4f720fe45abc26adda6397592d10b5d9
= <=>
2019-06-15 20:49:28 +0700

    abc

--- asm/js/1.js
+++ asm/js/1.js
@@ -1,10 +1,10 @@
-var hamburger = document.querySelector('.hamburger a');
-hamburger.addEventListener('click', e => {
+var bavach = document.getElementById('bavach');
+bavach.addEventListener('click', e => {
 	e.preventDefault();
 
 	e.target.classList.toggle('xoay');
 });
-
+// phần nút 3 vạch
 const search = document.getElementById('search').addEventListener('click', load);
 const form = document.querySelector('.search');
 
@@ -13,3 +13,19 @@ function load(e) {
 
 	form.classList.toggle('xuathien');
 }
+//phần form
+var header = document.getElementById('top');
+var trangthai = 1;
+window.addEventListener('scroll', function() {
+		if(window.pageYOffset >= 200) {
+			if (trangthai == 1) {
+				header.classList.add('cuon');
+				trangthai = 2;
+			}
+		} else if(trangthai == 2) {
+			header.classList.remove('cuon');
+			trangthai = 1;
+		}
+});
+
+
