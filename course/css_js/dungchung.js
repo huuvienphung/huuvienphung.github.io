b4741302a660b89db2bf0d39abfaaefca73ec127
= <=>
2019-08-08 21:14:33 +0700

    đf

--- course/css_js/dungchung.js
+++ course/css_js/dungchung.js
@@ -1,20 +1,43 @@
+// ajax
+var xhr = new XMLHttpRequest();
+
+xhr.open('GET', 'css_js/fours.json', true);
+xhr.onload = function() {
+	if(this.status == 200) {
+		var card = JSON.parse(this.responseText);
+
+		var output = '';
+
+		for (var i = 0; i < card.length; i++) {
+			output += `
+			<div class="card">
+			<h5 class="card-header">${card[i].td}</h5>
+			<div class="card-body">
+			<p class="card-text">${card[i].nd}</p>
+			</div>
+			</div>
+			`;
+		}
+		document.getElementById('fours').innerHTML = output;
+
+	}
+}
+xhr.send();
+
 
 const buttons = document.querySelectorAll('.nut');
 const noidungs = document.querySelectorAll('.noidung');
 const ul = document.querySelector('ul');
 const form = document.querySelector('.form-control')
 
+console.log(buttons);
+console.log(noidungs);
 
 // click vào hiện ra nội dung
-buttons.forEach(function(button) {
+buttons.forEach(function(button, index) {
 	button.addEventListener('click', (e) => {
 
-		if(e.target.classList.contains('nut')) {
-			e.target.parentElement.previousElementSibling.classList.add('xuathien');
-		} else {
-			e.target.parentElement.parentElement.previousElementSibling.classList.add('xuathien');
-		}
-		
+		noidungs[index].classList.add('xuathien');
 		document.body.style.overflow = 'hidden';
 		e.preventDefault();
 	})
@@ -72,29 +95,9 @@ apps.forEach(function(app) {
 	})
 })
 
-// ajax
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'css_js/fours.json', true);
-xhr.onload = function() {
-	if(this.status == 200) {
-		var card = JSON.parse(this.responseText);
 
-		var output = '';
 
-		for (var i = 0; i < card.length; i++) {
-			output += `
-			<div class="card">
-				<h5 class="card-header">${card[i].td}</h5>
-				<div class="card-body">
-					<p class="card-text">${card[i].nd}</p>
-				</div>
-			</div>
-		`;
-		}
-		document.getElementById('fours').innerHTML = output;
-		
-	}
-}
-xhr.send();
+
+
 		
 	
