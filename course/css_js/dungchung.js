50cd5f422df87685d66c21243e9e682ebd1ae440
= <=>
2019-08-09 20:14:45 +0700

    ádf

--- course/css_js/dungchung.js
+++ course/css_js/dungchung.js
@@ -39,10 +39,8 @@ form.addEventListener('keyup', e => {
 	})
 })
 
-const image = document.querySelectorAll('.appear .chon');
-for (var i = 0; i < image.length; i++) {
-	image[i].style.display = 'none';
-}
+
+
 
 // tạo các nút chọn
 const apps = document.querySelectorAll('.app');
@@ -51,26 +49,26 @@ apps.forEach(function(app) {
 	const luachons = app.querySelectorAll('.choosen a');
 	const chons = app.querySelectorAll('.appear .chon');
 	
-	for (let i = 0; i < chons.length; i++) {
+	for (var i = 1; i < chons.length; i++) {
 		chons[0].style.display = 'block';
 	}
 	
-	
-		luachons.forEach(function (luachon, ind) {
-			luachon.addEventListener('click', e => {
-				console.log(luachon);
-				
-				for (var i = 0; i < chons.length; i++) {
-					luachons[i].classList.remove('active');
-				}
-				e.target.classList.add('active');
-				for (var i = 0; i < chons.length; i++) {
-					chons[i].style.display = 'none';
-				}
-				chons[ind].style.display = 'block';
-				e.preventDefault();
-			})
+	luachons.forEach(function (luachon, ind) {
+		luachon.addEventListener('click', e => {
+			chons[0].style.display = 'block';
+			
+			for (var i = 0; i < chons.length; i++) {
+				luachons[i].classList.remove('active');
+			}
+			e.target.classList.add('active');
+			for (var i = 0; i < chons.length; i++) {
+				chons[i].style.display = 'none';
+			}
+			
+			chons[ind].style.display = 'block';
+			e.preventDefault();
 		})
+	})
 	
 	
 })
