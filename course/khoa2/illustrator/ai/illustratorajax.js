573faaa1b13cc6ac757b110c5080e27b9c908d8c
= <=>
2019-08-25 01:44:57 +0700

    adsf

---
+++ course/khoa2/illustrator/ai/illustratorajax.js
@@ -0,0 +1,96 @@
+var url = 'illustrator/ai/illustrator'; // link file data
+var url2 = 'illustrator/'; // link ảnh
+
+const datax = document.getElementById('data');
+
+var xhr = new XMLHttpRequest();
+
+xhr.open('GET', url+'data.json', true);
+xhr.onload = function() {
+	if(this.status == 200) {
+		
+		var data = JSON.parse(this.responseText);
+
+		var output = '';
+
+		for (var i = 0; i < data.length; i++) {
+			output += `
+				<tr>
+					<th scope="row">${i+1}</th>
+					<td>${data[i].tda}</td>
+					<td>${data[i].nd}</td>
+					<td><button data-index="${i}" class="btn btn-dark nut"><i class="fab fa-angular"></i></button></td>
+				</tr>
+			`;
+		}
+		datax.innerHTML = output;
+
+	}
+}
+
+xhr.send();
+
+
+data.addEventListener('click', e => {
+
+	if (e.target.classList.contains('nut')) {
+		var index = e.target.getAttribute('data-index');
+		loadData(index, url, url2);
+
+	} else if (e.target.classList.contains('fab')) {
+		var index = e.target.parentElement.getAttribute('data-index');
+		loadData(index, url, url2);
+	}
+})
+
+function loadData(index, url, url2) {
+	var xhr = new XMLHttpRequest();
+
+		xhr.open('GET', url+'data.json', true);
+
+		xhr.onload = function() {
+			if(this.status == 200) {
+
+				var data = JSON.parse(this.responseText);
+
+				var output = `
+					<div class="noidung xuathien">
+						<div class="container khung2">
+							<div class="card khoi2">
+								<div class="card-body app">
+									<h5 class="card-title">${data[index].tda}</h5>
+									<ul class="choosen">
+				`;
+				output+=`<li><a class="abc active" data-vt="0" href="#">${data[index].nut[0]}</a></li>`;
+				for (var i = 1; i < data[index].nut.length; i++) {
+					output+= `
+						<li><a class="abc" data-vt="${i}" href="#">${data[index].nut[i]}</a></li>
+					`;
+				}
+
+				output += `</ul><div class="appear">`;
+
+				for (var i = 0; i < data[index].noidung.length; i++) {
+					output +=`<div class="chon">`;
+					for (var j = 0; j < data[index].noidung[i].noidun.length; j++) {
+						output += `
+							<figure class="text">${data[index].noidung[i].noidun[j].td}</figure>
+							<div class="card-text"><img src="${url2}${data[index].noidung[i].noidun[j].anh}" alt="ảnh minh họa"></div>
+						
+					`
+					}
+					output+=`</div>`;
+				}
+
+				output+=`</div></div></div></div>`;
+				
+				document.getElementById('nd').innerHTML = output;
+				}
+
+
+				
+			}
+			xhr.send();
+		document.body.style.overflow = 'hidden';
+		
+}
