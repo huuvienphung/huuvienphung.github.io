ad61a53edbde85a4dad6de8db9fd0381160370c5
= <=>
2019-09-05 06:22:55 +0700

    sdf

--- duan/test-11/app.js
+++ duan/test-11/app.js
@@ -1,6 +1,7 @@
 const box = document.querySelector('.box-pro');
 const funlist = [];
 let myBlock, myButton;
+const randomArray = ['left','top','right','down'];
 document.addEventListener('DOMContentLoaded', function() {
 	myBlock = document.createElement('div');
 	myBlock.textContent = 'Hello world';
@@ -15,15 +16,22 @@ document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener('keydown', function(e) {
 	e.preventDefault();
 	let el = e.keyCode;
+	console.log(el);
 	switch (el) {
-		case 37: addFun('left'); break;
-		case 38: addFun('top'); break;
-		case 39: addFun('right'); break;
-		case 40: addFun('down'); break;
+		case 37: addFun('left'); break; // left
+		case 38: addFun('top'); break; // top
+		case 39: addFun('right'); break; // right
+		case 40: addFun('down'); break; // down
+		case 82:{  // r
+			let ran = Math.floor(Math.random() * randomArray.length);
+			let temp = randomArray[ran];
+			addFun(temp);
+			break;
+		}
 		case 67: myBlock.style.backgroundColor = randomColor(); break;
 		default: break;
 	}
-	if (el === 13 || el === 32) {
+	if (el === 13 || el === 32) { // backspace or enter
 		mover();
 	}
 })
@@ -32,11 +40,9 @@ function mover() {
 		let cur = myBlock.getBoundingClientRect();
 		let el = funlist.shift();
 		let item = el.textContent.replace('+','');
-		myButton.removeChild(myButton.childNodes[0]);
+		myButton.removeChild(el);
 		myBlock.textContent = 'Move: '+item;
-
-		console.log(item);
-		console.log(cur.left,cur.width);
+		
 		switch (item) {
 			case 'left':
 				myBlock.style.left = cur.left-cur.width+"px";
@@ -53,6 +59,9 @@ function mover() {
 				break;
 		}
 		setTimeout(mover, 1000);
+	} else {
+		myBlock.textContent = 'hello world';
+		return;
 	}
 }
 function addFun(val) {
@@ -67,6 +76,11 @@ function addFun(val) {
 	span.addEventListener('mouseout', function() {
 		this.style.backgroundColor = 'white';
 		this.style.color = 'black';
+	})
+	span.addEventListener('click', function() {
+		let curIndex = funlist.indexOf(this);
+		let tempRemove = funlist.splice(curIndex, 1);
+		myButton.removeChild(this);
 	})
 	myButton.appendChild(span);
 	funlist.push(span);
