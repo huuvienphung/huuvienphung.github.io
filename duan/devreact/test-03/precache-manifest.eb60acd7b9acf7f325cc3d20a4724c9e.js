33c6485f75739289b5d97093b95a52947fab3fd1
= <=>
2019-09-19 18:18:11 +0700

    adsf

---
+++ duan/devreact/test-03/precache-manifest.eb60acd7b9acf7f325cc3d20a4724c9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0142d187e5774d966fe420d3771fdc4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7db8f83bb35b011d7ea9",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "4501eef8e58b69d47bd5",
+    "url": "/static/js/2.1e3d5397.chunk.js"
+  },
+  {
+    "revision": "7db8f83bb35b011d7ea9",
+    "url": "/static/js/main.d0335295.chunk.js"
+  },
+  {
+    "revision": "c9163fb7a936a16aaa2b",
+    "url": "/static/js/runtime~main.06edc144.js"
+  }
+]);
